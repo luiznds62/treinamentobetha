a9b48a6b183fee9743956fef3ac0af93c7786b33
luiz.santos <luiz.santos@betha.com.br>
2019-11-25 17:33:51 -0300

    v

--- main.js
+++ main.js
@@ -1,35 +1,11 @@
-var url = "http://192.168.41.2:3000";
-
-function getGroovyDoc() {
-  fetch(url + "/groovy")
-    .then(resp => resp.blob())
-    .then(blob => {
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.style.display = "none";
-      a.href = url;
-      // the filename you want
-      a.download = "groovy.pdf";
-      document.body.appendChild(a);
-      a.click();
-      window.URL.revokeObjectURL(url);
-    })
-    .catch(() => console.log("erro"));
+function mostraModal(idModal) {
+  var display = document.querySelector(`#${idModal}`).style.display;
+  if (display === "none") {
+    var display = (document.querySelector(`#${idModal}`).style.display =
+      "block");
+  }
 }
 
-function getGerenc() {
-    fetch(url + "/gerenciadorcomponente")
-      .then(resp => resp.blob())
-      .then(blob => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.style.display = "none";
-        a.href = url;
-        // the filename you want
-        a.download = "gerenciadorcomponente.pdf";
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-      })
-      .catch(() => console.log("erro"));
-  }
+function escondeModal(idModal) {
+  document.querySelector(`#${idModal}`).style.display = "none";
+}
