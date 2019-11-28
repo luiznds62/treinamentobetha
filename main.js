db3bf7de8ac77b2ae48131f6f312a2a2541252a9
luiz.santos <luiz.santos@betha.com.br>
2019-11-28 15:40:06 -0300

    pes

--- main.js
+++ main.js
@@ -10,12 +10,53 @@ function escondeModal(idModal) {
   document.querySelector(`#${idModal}`).style.display = "none";
 }
 
-function abreDivCronograma(){
-  document.querySelector("#cronograma").style.display = 'block'
-  document.querySelector("#pesquisa").style.display = 'none'
+function abreDivCronograma() {
+  document.querySelector("#cronograma").style.display = "block";
+  document.querySelector("#pesquisa").style.display = "none";
 }
 
-function abreDivPesquisa(){
-  document.querySelector("#cronograma").style.display = 'none'
-  document.querySelector("#pesquisa").style.display = 'block'
+function abreDivPesquisa() {
+  document.querySelector("#cronograma").style.display = "none";
+  document.querySelector("#pesquisa").style.display = "block";
+}
+
+function enviarPesquisa() {
+  var nome = $("#formNome").val();
+  var pergunta1 = $("#formPergunta1")
+    .find(":selected")
+    .val();
+  var pergunta2 = $("#formPergunta2")
+    .find(":selected")
+    .val();
+  var nota = $("#formNota")
+    .find(":selected")
+    .val();
+  var comentario = $("#formComentario").val();
+
+  if (!nome) {
+    alert("É necessário informar o nome");
+    return;
+  }
+
+  $.post(
+    "https://pesquisasatisfacao.herokuapp.com/",
+    {
+      nome: nome,
+      pergunta1: pergunta1,
+      pergunta2: pergunta2,
+      nota: nota,
+      comentario: comentario
+    },
+    function(result) {
+      if (result === "Sucesso!") {
+        alert(result);
+        $("#formNome").val("");
+        $("#formComentario").val("");
+        $("#formPergunta1").prop('selectedIndex',0);
+        $("#formPergunta2").prop('selectedIndex',0);
+      } else {
+        alert("Erro");
+      }
+    }
+  );
 }
